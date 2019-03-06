1cf6a1e75c3eddaf71f82b52705e6e65de5dd44b
kikemadrigal <kikemadrigal@hotmail.com>
2019-03-06 23:38:39 +0100

    primer commit

---
+++ app/src/main/java/es/tipolisto/fotomapajava/ForeCast.java
@@ -0,0 +1,29 @@
+package es.tipolisto.fotomapajava;
+
+public class ForeCast {
+    private String cod;
+    private City city;
+    public ForeCast(){}
+
+    public ForeCast(String cod, City city) {
+        this.cod = cod;
+        this.city = city;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+}
---
+++ app/src/main/java/es/tipolisto/fotomapajava/IWeatherService.java
@@ -0,0 +1,11 @@
+package es.tipolisto.fotomapajava;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IWeatherService {
+    //forecast?id=524901&appid=7a92dffe3be099c10e08312ccbd38c68
+    @GET("forecast")
+    Call<ForeCast> getCity(@Query("id") String city, @Query("appid") String key);
+}
